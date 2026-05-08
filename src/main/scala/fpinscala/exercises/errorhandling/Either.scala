d28599684230c504e3f9065c325024f202e94d34
Rachel Wong <rachel.wong62@gmail.com>
2026-05-08 19:32:54 +0200

    round off Either exercises

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -31,7 +31,7 @@ enum Either[+E, +A]:
 
 object Either:
   def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
-    es.foldRight[Either[E, List[B]]](Right(Nil))(f(_).map2(_)(_ :: _))
+    es.foldRight(Right(Nil): Either[E, List[B]])(f(_).map2(_)(_ :: _))
 
   def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
     traverse(es)(x => x)
@@ -52,13 +52,21 @@ object Either:
       a: Either[List[E], A],
       b: Either[List[E], B],
       f: (A, B) => C
-  ): Either[List[E], C] = ???
+  ): Either[List[E], C] = (a, b) match
+    case (Right(aa), Right(bb)) => Right(f(aa, bb))
+    case (Left(es), Right(_))   => Left(es)
+    case (Right(_), Left(es))   => Left(es)
+    case (Left(e1), Left(e2))   => Left(e1 ++ e2)
 
   def traverseAll[E, A, B](
       as: List[A],
       f: A => Either[List[E], B]
-  ): Either[List[E], List[B]] = ???
+  ): Either[List[E], List[B]] =
+    as.foldRight(Right(Nil): Either[List[E], List[B]])((a, acc) =>
+      map2All(f(a), acc, _ :: _)
+    )
 
   def sequenceAll[E, A](
       as: List[Either[List[E], A]]
-  ): Either[List[E], List[A]] = ???
+  ): Either[List[E], List[A]] =
+    traverseAll(as, x => x)
