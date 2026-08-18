03a5d7bd2e04a46335b1b327de007b19d4b6ec43
Rachel Wong <rachel.wong62@gmail.com>
2026-08-18 16:28:30 +1000

    Implement Gen exercises

--- src/main/scala/fpinscala/exercises/testing/Gen.scala
+++ src/main/scala/fpinscala/exercises/testing/Gen.scala
@@ -21,14 +21,30 @@ opaque type Gen[+A] = State[RNG, A]
 
 object Gen:
 
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = State.unit(a)
 
-  def choose(start: Int, stopExclusive: Int): Gen[Int] = ???
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start))
+
+  def boolean: Gen[Boolean] =
+    State(RNG.nonNegativeInt).map(n => n % 2 == 0)
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    Gen.boolean.flatMap(b => if b then g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] =
+    val max = 10000
+    val threshold = max * g1(1) / (g1(1) + g2(1))
+    Gen.choose(0, max).flatMap(n => if n >= threshold then g2(0) else g1(0))
 
   extension [A](self: Gen[A])
-    def flatMap[B](f: A => Gen[B]): Gen[B] = ???
-
     // We should use a different method name to avoid looping (not 'run')
     def next(rng: RNG): (A, RNG) = self.run(rng)
+
+    def flatMap[B](f: A => Gen[B]): Gen[B] = State.flatMap(self)(f)
+
+    def listOfN(n: Int): Gen[List[A]] = State.sequence(List.fill(n)(self))
+
+    def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(listOfN)
 
 trait SGen[+A]
